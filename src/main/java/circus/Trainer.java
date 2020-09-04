1f4a586688738c130086d1068211870e5705760c
jiaaaqi <e0425649@u.nus.edu>
2020-09-04 17:24:36 +0800

    add changes done in lecture

---
+++ src/main/java/circus/Trainer.java
@@ -0,0 +1,37 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
+public class Trainer {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+        getToSpeak(d);
+
+        Bird b = (Bird)d;  // upcasting
+        getToSpeak(b);
+
+        Animal a = (Animal)b; // upcasting
+        getToSpeak(a);
+
+        Duck d2 = (Duck) a; // downcasting: convert superclass object to child class
+        getToSpeak(d2);
+        train(new Duck());
+        train(new Parrot());
+        // circus.animal.Animal a2 = new circus.animal.Animal();
+        // circus.animal.Bird b2 = new circus.animal.Bird();
+    }
+
+    private static void getToSpeak(Animal animal) {
+        System.out.println(animal.speak());
+    }
+
+    private static void train(Bird bird) {
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        }
+    }
+}
